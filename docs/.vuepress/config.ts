380bcab81f4e37de0545e3a48c3c06397d1c20d5
qouson <791146450@qq.com>
2024-11-14 17:42:53 +0800

    修改base

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -22,7 +22,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       description: 'Java技术博客,专注Java学习与总结。',
     }
   },
-  // base: '/my-Java-note/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+  base: '/my-Java-note/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
 
   // 主题配置
   themeConfig: {
